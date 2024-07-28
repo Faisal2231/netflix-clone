6194c772d1c3fd8f58fe023591132a6ec6e0ce01
Mohd <mohdfaisal2140@gmail.com>
2024-07-28 16:17:08 +0530

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCMNp440phzxi33llNtFTmkvXfKUCq2Xe4",
+  authDomain: "netflix-clone-66802.firebaseapp.com",
+  projectId: "netflix-clone-66802",
+  storageBucket: "netflix-clone-66802.appspot.com",
+  messagingSenderId: "109930867248",
+  appId: "1:109930867248:web:6c5c5b209a5d6d12325bf5",
+};
